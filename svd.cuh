1d584fffcdba7a5867fe52082ba5bc88595743d8
Hammad Mazhar <hammad.maz@gmail.com>
2015-11-10 11:47:12 -0600

    update gpu_math version and modify code as needed

--- svd.cuh
+++ svd.cuh
@@ -7,7 +7,7 @@
 // This code is based on code from Physbam
 
 // Oliver K. Smith. 1961. Eigenvalues of a symmetric 3 × 3 matrix. Commun. ACM 4, 4 (April 1961), 168-. DOI=http://dx.doi.org/10.1145/355578.366316
-__host__ __device__ real3 Fast_Eigenvalues(const SymMat33& A) 
+CUDA_HOST_DEVICE __noinline__ real3 Fast_Eigenvalues(const SymMat33& A)  // 24 mults, 20 adds, 1 atan2, 1 sincos, 2 sqrts
 {
     real m = 1.0f / 3.0f * (A.x11 + A.x22 + A.x33);
     real a11 = A.x11 - m;
@@ -30,36 +30,38 @@ __host__ __device__ real3 Fast_Eigenvalues(const SymMat33& A)
     return lambda;
 }
 
-__host__ __device__ Mat33 Fast_Eigenvectors(const SymMat33& A, real3& lambda) {
+CUDA_HOST_DEVICE Mat33 __noinline__ Fast_Eigenvectors(const SymMat33& A, real3& lambda) {
     // flip if necessary so that first eigenvalue is the most different
     bool flipped = false;
     real3 lambda_flip(lambda);
-    if (lambda.x - lambda.y < lambda.y - lambda.z) {
+    if (lambda.x - lambda.y < lambda.y - lambda.z) {  // 2a
         Swap(lambda_flip.x, lambda_flip.z);
         flipped = true;
     }
 
     // get first eigenvector
-    real3 v1 = Largest_Column_Normalized(Cofactor_Matrix(A - lambda_flip.x));
+    real3 v1 = LargestColumnNormalized(CofactorMatrix(A - lambda_flip.x));  // 3a + 12m+6a + 9m+6a+1d+1s = 21m+15a+1d+1s
     // form basis for orthogonal complement to v1, and reduce A to this space
-    real3 v1_orthogonal = UnitOrthogonalVector(v1);
-    Mat32 other_v(v1_orthogonal, Cross(v1, v1_orthogonal));
-    SymMat22 A_reduced = Conjugate_With_Transpose(other_v, A);
-
+    real3 v1_orthogonal = UnitOrthogonalVector(v1);             // 6m+2a+1d+1s (tweak: 5m+1a+1d+1s)
+    Mat32 other_v(v1_orthogonal, Cross(v1, v1_orthogonal));     // 6m+3a (tweak: 4m+1a)
+    SymMat22 A_reduced = ConjugateWithTranspose(other_v, A);  // 21m+12a (tweak: 18m+9a)
     // find third eigenvector from A_reduced, and fill in second via cross product
-    real3 v3 = other_v * Largest_Column_Normalized(Cofactor_Matrix(A_reduced - lambda_flip.z));
 
-    real3 v2 = Cross(v3, v1);
+    // 6m+3a + 2a + 5m+2a+1d+1s = 11m+7a+1d+1s (tweak: 10m+6a+1d+1s)
 
+    real3 v3 = other_v * LargestColumnNormalized(CofactorMatrix(A_reduced - lambda_flip.z));
+
+    real3 v2 = Cross(v3, v1);  // 6m+3a
+    // finish
     return flipped ? Mat33(v3, v2, -v1) : Mat33(v1, v2, v3);
 }
 
-__host__ __device__ void Fast_Solve_EigenProblem(const SymMat33& A, real3& eigen_values, Mat33& eigen_vectors) {
+CUDA_HOST_DEVICE void Fast_Solve_EigenProblem(const SymMat33& A, real3& eigen_values, Mat33& eigen_vectors) {
     eigen_values = Fast_Eigenvalues(A);
     eigen_vectors = Fast_Eigenvectors(A, eigen_values);
 }
 
-__host__ __device__ void SVD(const Mat33& A, Mat33& U, real3& singular_values, Mat33& V) {
+CUDA_HOST_DEVICE void SVD(const Mat33& A, Mat33& U, real3& singular_values, Mat33& V) {
     SymMat33 ATA = NormalEquationsMatrix(A);
     real3 lambda;
     Fast_Solve_EigenProblem(ATA, lambda, V);
@@ -73,12 +75,13 @@ __host__ __device__ void SVD(const Mat33& A, Mat33& U, real3& singular_values, M
     }
 
     // compute singular vectors
-    U.cols[0] = Normalize(A * V.cols[0]);
-    real3 v1 = UnitOrthogonalVector(U.cols[0]);
-    real3 v2 = Cross(U.cols[0], v1);
+    U.cols[0] = Normalize(A * V.cols[0]);        // 15m+8a+1d+1s
+    real3 v1 = UnitOrthogonalVector(U.cols[0]);  // 6m+2a+1d+1s
+    real3 v2 = Cross(U.cols[0], v1);             // 6m+3a
 
+    // 6m+3a + 6m+4a + 9m+6a + 6m+2a+1d+1s = 27m+15a+1d+1s
     real3 v3 = A * V.cols[1];
     real2 other_v = Normalize(real2(Dot(v1, v3), Dot(v2, v3)));
     U.cols[1] = real3(v1.x * other_v.x + v2.x * other_v.y, v1.y * other_v.x + v2.y * other_v.y, v1.z * other_v.x + v2.z * other_v.y);
-    U.cols[2] = Cross(U.cols[0], U.cols[1]);
+    U.cols[2] = Cross(U.cols[0], U.cols[1]);  // 6m+3a
 }
