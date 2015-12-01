02422d0c66ef574e2d86c19ea97fa08c783b8186
Hammad Mazhar <hammad.maz@gmail.com>
2015-12-01 12:40:40 -0600

    update main file with examples

--- main.cu
+++ main.cu
@@ -6,14 +6,34 @@
 #include "svd.cuh"
 
 Mat33 A(real3(10, 7, -10), real3(-9, -12, 10), real3(-12, 11, 3));
-
+Mat33 B(real3(1, 0, 0), real3(0, 1, 0), real3(0, 0, 1));
 int main() {
     Mat33 U, V;
     real3 singular_values;
 
     SVD(A, U, singular_values, V);
+    printf("SVD (A): \n");
+    printf("%f %f %f \n", U.cols[0].x, U.cols[1].x, U.cols[2].x);
+    printf("%f %f %f \n", U.cols[0].y, U.cols[1].y, U.cols[2].y);
+    printf("%f %f %f \n", U.cols[0].z, U.cols[1].z, U.cols[2].z);
+    printf("=========\n");
+    printf("%f %f %f \n", V.cols[0].x, V.cols[1].x, V.cols[2].x);
+    printf("%f %f %f \n", V.cols[0].y, V.cols[1].y, V.cols[2].y);
+    printf("%f %f %f \n", V.cols[0].z, V.cols[1].z, V.cols[2].z);
+    printf("=========\n");
+    printf("%f %f %f \n", singular_values.x, singular_values.y, singular_values.z);
 
-    printf("%f %f %f", singular_values.x, singular_values.y, singular_values.z);
+ 	SVD(B, U, singular_values, V);
+printf("SVD (B): \n");
+    printf("%f %f %f \n", U.cols[0].x, U.cols[1].x, U.cols[2].x);
+    printf("%f %f %f \n", U.cols[0].y, U.cols[1].y, U.cols[2].y);
+    printf("%f %f %f \n", U.cols[0].z, U.cols[1].z, U.cols[2].z);
+    printf("=========\n");
+    printf("%f %f %f \n", V.cols[0].x, V.cols[1].x, V.cols[2].x);
+    printf("%f %f %f \n", V.cols[0].y, V.cols[1].y, V.cols[2].y);
+    printf("%f %f %f \n", V.cols[0].z, V.cols[1].z, V.cols[2].z);
+    printf("=========\n");
+    printf("%f %f %f \n", singular_values.x, singular_values.y, singular_values.z);
 
     return 0;
 }
